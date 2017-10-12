6ac475db1695813f3fa8773b29fa7bc7aa21782d
Marko Kosunen <marko.kosunen@aalto.fi>
2017-10-11 18:02:35 -0700

    Channel model correlation matrix computation ready
    TODO-Add line of sight

--- py/f2_channel.py
+++ py/f2_channel.py
@@ -1,13 +1,17 @@
 # f2_channel class 
 #Thechannel model in this module is based on 802.11n channel models decribed in
 # IEEE 802.11n-03/940r4 TGn Channel Models
-# Last modification by Marko Kosunen, marko.kosunen@aalto.fi, 10.10.2017 18:11
+# Last modification by Marko Kosunen, marko.kosunen@aalto.fi, 11.10.2017 17:46
+import sys
+sys.path.append ('/home/projects/fader/TheSDK/Entities/refptr/py')
+sys.path.append ('/home/projects/fader/TheSDK/Entities/thesdk/py')
 import numpy as np
+from numpy.lib import scimath as scm
 import scipy.linalg as sli
-import tempfile
-import subprocess
-import shlex
-import time
+#import tempfile
+#import subprocess
+#import shlex
+#import time
 
 from refptr import *
 from thesdk import *
@@ -17,43 +21,54 @@ def laplacian_pdf(sigma,theta):
     Q=1/(1-np.exp(-np.sqrt(2)*(theta[-1]-theta[0])))
     #Q=1
     PAS=Q*np.exp(-np.sqrt(2)*np.abs(theta)/sigma)/(np.sqrt(2)*sigma)
-    return PAS
+    return PAS.reshape((1,-1))
 
-def corrm(d,f,sigma,AoA):
+def corrm(*arg):
+     #Is the dict better way to apss the arguments
      #Models defined only in the horizontal plane
      # IEEE 802.11n-03/940r4 TGn Channel Models
      #d is a Mx1 distance vector between antenna elements
      #f is the transmission frequency
      #sigma is the angular spread (standard deviation in radians.
      #AoA is th angle of arrival in radians
+     #Add the receiver array at some point
+     d1=arg[0]     # Distance array for the receiver antenna
+     f=arg[1]     # Frequency
+     sigmarx=arg[2] # Angle spread for the receiver
+     AoA=arg[3]   #Angle of arrival for received
      c=299792458 #Speed of light, m/s
      lamda=c/f 
-     dmat=sli.toeplitz(d,d)
-     M=dmat.shape[0] # Number of antennas
-     D=2*np.pi*dmat/lamda
+     dmatrx=sli.toeplitz(d1,d1)
+     M1=dmatrx.shape[0] # Number of receive antennas
+     M2=1 #number of transmit antennas
+     Drx=2*np.pi*dmatrx/lamda
      #RXX integ | phi -pi -pi cos(d*np.sin(phi)*laplacian(sigma,phi)dphi
      #RXY integ | phi -pi -pi sin(d*np.sin(phi)*laplacian(sigma,phi)dphi
      #Combine these to matrix
-     phirange=np.linspace(-np.pi,np.pi,2**16)+AoA
-     dphi=np.diff(phirange)[0]
+     phirangerx=np.linspace(-np.pi,np.pi,2**16)+AoA
+     dphirx=np.diff(phirangerx)[0]
      #There's an error due to numerical integration. With angle 0 the correlation must be 1
      #calculate that
-     Kcorr=1/(np.sum(laplacian_pdf(sigma,phirange-AoA))*dphi)
-     laplacianweightmat=np.ones((M,1))*laplacian_pdf(sigma,phirange-AoA)
-     R=np.zeros((M,M),dtype='complex')
-     for i in range(M): 
-         R[i,:]=Kcorr*np.sum(np.exp(1j*D[i,:].reshape((-1,1))*np.sin(phirange))*laplacianweightmat,1)*dphi
-     return R
+     Kcorrrx=1/(np.sum(laplacian_pdf(sigma,phirangerx-AoA))*dphirx)
+     laplacianweightmatrx=np.ones((M1,1))@laplacian_pdf(sigmarx,phirangerx-AoA)
+     Rrx=np.zeros((M1,M1),dtype='complex')
+     for i in range(M1): 
+         Rrx[i,:]=Kcorrrx*np.sum(np.exp(1j*Drx[i,:].reshape((-1,1))*np.sin(phirangerx))*laplacianweightmatrx,1)*dphirx
+     #Would require similar computations if the TX would be modeled
+     Rtx=np.diagflat(np.ones((M2,1)))
+     #Random matrix
+     Hiid=1/np.sqrt(2)*(np.random.randn(M1,M2)+1j*np.random.rand(M1,M2))
+     #Correlation matrix 
+     X=scm.sqrt(Rrx)@Hiid@scm.sqrt(Rtx)
+     return X
+
+
 
 def lambda2meter(distlambda,f):
     c=299792458 
     d=distlambda*c/f
     return d
 
-def gen_dist_mat(d):
-    #Missing scipy. fast replacement
-    distmat=np.zeros((d.shape[0],d.shape[0]))
-    distmat=
 
 #Simple buffer template
 class f2_channel(thesdk):
@@ -128,3 +143,16 @@ class f2_channel(thesdk):
         else:
             loss=1/(4*np.pi*(distance)*frequency/c)**2
         return loss
+    
+if __name__=="__main__":
+    import sys
+    f=1e9
+    AoA=2*np.pi*30/360
+    sigma=2*np.pi*30/360
+    d=lambda2meter(np.array(range(4)),f)
+    X=corrm(d,f,sigma,AoA)
+    print(X)
+    print(X.shape)
+    print(np.abs(X))
+    #print(Rrx)
+
