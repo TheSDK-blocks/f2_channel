f2a876c4cb7e5b357cd056f2ea07a74f51775937
Marko Kosunen <marko.kosunen@aalto.fi>
2017-10-17 14:33:17 -0700

    Channel models buffer buffer_1km, 802_11n_A, B, C, D, and E simulates

--- py/f2_channel.py
+++ py/f2_channel.py
@@ -1,7 +1,7 @@
 # f2_channel class 
 #Thechannel model in this module is based on 802.11n channel models decribed in
 # IEEE 802.11n-03/940r4 TGn Channel Models
-# Last modification by Marko Kosunen, marko.kosunen@aalto.fi, 16.10.2017 20:35
+# Last modification by Marko Kosunen, marko.kosunen@aalto.fi, 17.10.2017 14:22
 import sys
 sys.path.append ('/home/projects/fader/TheSDK/Entities/refptr/py')
 sys.path.append ('/home/projects/fader/TheSDK/Entities/thesdk/py')
@@ -199,13 +199,11 @@ def generate_802_11n_channel(*arg): #{'Rs': 'model': 'Rxantennalocations': 'freq
     #Let Angle of departure be o degrees by, and angle of arrival be random
     channel_param_dict['AoD']=np.zeros(channel_param_dict['AoD'].shape)
     shape=channel_param_dict['AoA'].shape
-    print(shape)
     channel_param_dict['AoA']=np.random.rand(shape[0],shape[1])*360
 
     #For each channel there are multiple clusters of taps
     for cluster_index in range(channel_param_dict['AoA'].shape[0]):
         #taps inside the cluster
-        print(channel_param_dict['tau'].shape[0])
         for tap_index in range(tau.shape[0]):
             tapdict={'Rxantennalocations': Rxantennalocations, 'frequency': frequency, 'K':channel_param_dict['K'][tap_index], 
                     'tau':channel_param_dict['tau'][tap_index], 'pdb':channel_param_dict['pdb'][cluster_index][tap_index], 
@@ -345,20 +343,15 @@ def get_802_11n_channel_params(model):
 # This function hard-codes the WLAN 802.11n channel model parameters and
 # returns the ones corresponding to the desired channel model. 
 #param_dict={'K':K, 'tau':tau, 'pdb':pdb, 'AS_Tx':AS_Tx, 'AoD':AoD, 'AS_Rx':AS_Rx, 'AoA':AoA}
-
+#The IS a more clever way of doing these but at least they are done now.
     if model=='A':
         tau = np.array([0])
         K=np.zeros(tau.size) #K-factor for Line-of-sight
-        pdb = np.array([0])
-        pdb.shape=(1,1)
-        AoA = np.array([45])
-        AoA.shape=(1,1)
-        AS_Rx = np.array([40])
-        AS_Rx.shape=(1,1)
-        AoD = np.array([45])
-        AoD.shape=(1,1)
-        AS_Tx = np.array([40])
-        AS_Tx.shape=(1,1)
+        pdb = np.array([0],ndmin=2)
+        AoA = np.array([45],ndmin=2)
+        AS_Rx = np.array([40],ndmin=2)
+        AoD = np.array([45],ndmin=2)
+        AS_Tx = np.array([40],ndmin=2)
         
     elif model=='B':
         
@@ -366,180 +359,242 @@ def get_802_11n_channel_params(model):
         K=np.zeros(tau.size) #K-factor for Line-of-sight
         
         # Average path gains of cluster, in dB
-        pdb1 = np.array([0,-5.4,-10.8,-16.2,-21.7, -1*np.inf,-1*np.inf,-1*np.inf,-1*np.inf],ndmin=2)
-        pdb2 = np.array([-1*np.inf,-1*np.inf,-3.2,-6.3,-9.4,-12.5,-15.6,-18.7,-21.8],ndmin=2)
-
+        pdb1 = np.array([0,-5.4,-10.8,-16.2,-21.7],ndmin=2)
+        pdb2 = np.array([-3.2,-6.3,-9.4,-12.5,-15.6,-18.7,-21.8],ndmin=2)
+        #these must be reshaped last because others refer their dimensions
+     
         #   Angular spreads
-        AS_Tx_C1 = np.array([14.4,14.4,14.4,14.4,14.4,-1*np.inf,-1*np.inf,-1*np.inf,-1*np.inf],ndmin=2)
-        AS_Tx_C2 = np.array([-1*np.inf,-1*np.inf,25.4,25.4,25.4,25.4,25.4,25.4,25.4],ndmin=2)
+        AS_Tx_C1 = np.ones(pdb1.shape)*14.4
+        AS_Tx_C1 = np.r_['1', AS_Tx_C1, -1*np.inf*np.ones((1,4))]
+
+        AS_Tx_C2 = np.ones(pdb2.shape)*25.4
+        AS_Tx_C2 = np.r_['1', -1*np.inf*np.ones((1,2)), AS_Tx_C2 ]
+        AS_Tx = np.r_['0', AS_Tx_C1, AS_Tx_C2]
+
         #   Mean angles of departure
-        AoD_C1 = np.array([225.1,225.1,225.1,225.1,225.1,-1*np.inf,-1*np.inf,-1*np.inf,-1*np.inf],ndmin=2)
-        AoD_C2 = np.array([-1*np.inf,-1*np.inf,106.5,106.5,106.5,106.5,106.5,106.5,106.5],ndmin=2)
+        AoD_C1 = np.ones(pdb1.shape)*225.1
+        AoD_C1 = np.r_['1', AoD_C1, -1*np.inf*np.ones((1,4))]
+
+        AoD_C2 = np.ones(pdb2.shape)*106.5
+        AoD_C2 = np.r_['1', -1*np.inf*np.ones((1,2)), AoD_C2 ]
+
+        AoD = np.r_['0',AoD_C1, AoD_C2]
 
         # Spatial parameters on receiver side:
         #   Angular spreads
-        AS_Rx_C1 = np.array([14.4,14.4,14.4,14.4,14.4,-1*np.inf,-1*np.inf,-1*np.inf,-1*np.inf],ndmin=2)
-        AS_Rx_C2 = np.array([-1*np.inf,-1*np.inf,25.2,25.2,25.2,25.2,25.2,25.2,25.2],ndmin=2)
+        AS_Rx_C1 = np.ones(pdb1.shape)*14.4
+        AS_Rx_C1 = np.r_['1', AS_Rx_C1, -1*np.inf*np.ones((1,4))]
+
+        AS_Rx_C2 = np.ones(pdb2.shape)*25.4
+        AS_Rx_C2 = np.r_['1', -1*np.inf*np.ones((1,2)), AS_Rx_C2 ]
+
+        AS_Rx = np.r_['0', AS_Rx_C1, AS_Rx_C2]
 
         #   Mean angles of arrival
-        AoA_C1 = np.array([4.3,4.3,4.3,4.3,4.3,-1*np.inf,-1*np.inf,-1*np.inf,-1*np.inf],ndmin=2)
-        AoA_C2 = np.array([-1*np.inf,-1*np.inf,118.4,118.4,118.4,118.4,118.4,118.4,118.4],ndmin=2)
+        AoA_C1 = np.ones(pdb1.shape)*4.3
+        AoA_C2 = np.ones(pdb2.shape)*118.4
         
-        pdb = np.r_['0',pdb1,pdb2]
-        AS_Tx = np.r_['0', AS_Tx_C1, AS_Tx_C2]
-        AoD = np.r_['0',AoD_C1, AoD_C2]
-        AS_Rx = np.r_['0', AS_Rx_C1, AS_Rx_C2]
+        AoA_C1 = np.r_['1', AoA_C1, -1*np.inf*np.ones((1,4))]
+        AoA_C2 = np.r_['1', -1*np.inf*np.ones((1,2)), AoA_C2 ]
+
         AoA = np.r_['0', AoA_C1, AoA_C2]
-        
+
+        #Reshape pdb's
+        pdb1 = np.r_['1', pdb1, -1*np.inf*np.ones((1,4))]
+        pdb2 = np.r_['1', -1*np.inf*np.ones((1,2)), pdb2 ]
+        pdb = np.r_['0',pdb1,pdb2]
+       
     elif model=='C':
         
         tau = np.array([0,10,20,30,40,50,60,70,80,90,110,140,170,200]) * 1e-9
         K=np.zeros(tau.size) #K-factor for Line-of-sight
         
         pdb1 = np.array([0,-2.1,-4.3,-6.5,-8.6,-10.8,-13.0,-15.2,-17.3,-19.5],ndmin=2)
-        AoA1 = 290.3*np.ones(pdb1.shape)
-        AS_Rx1 = 24.6*np.ones(pdb1.shape)
-        AoD1 = 13.5*np.ones(pdb1.shape)
-        AS_Tx1 = 24.7*np.ones(pdb1.shape)
-        
         pdb2 = np.array([-5.0,-7.2,-9.3,-11.5,-13.7,-15.8,-18.0,-20.2],ndmin=2)
-        AoA2 = 332.3*np.ones(pdb2.shape)
-        AS_Rx2 = 22.4*np.ones(pdb2.shape)
-        AoD2 = 56.4*np.ones(pdb2.shape)
-        AS_Tx2 = 22.5*np.ones(pdb2.shape)
-        
-        pdb1 = np.r_['1',pdb1,-1*np.inf*np.ones((1,4))]
+
+
+        AoA1 = 290.3*np.ones(pdb1.shape)
         AoA1 = np.r_['1',AoA1,-1*np.inf*np.ones((1,4))]
+
+        AoA2 = 332.3*np.ones(pdb2.shape)
+        AoA2 = np.r_['1',-1*np.inf*np.ones((1,6)),AoA2]
+
+        AoA = np.r_['0',AoA1, AoA2]
+
+        AS_Rx1 = 24.6*np.ones(pdb1.shape)
         AS_Rx1 = np.r_['1',AS_Rx1,-1*np.inf*np.ones((1,4))]
+        
+        AS_Rx2 = 22.4*np.ones(pdb2.shape)
+        AS_Rx2 = np.r_['1',-1*np.inf*np.ones((1,6)),AS_Rx2]
+
+        AS_Rx = np.r_['0',AS_Rx1, AS_Rx2]
+        
+        AoD1 = 13.5*np.ones(pdb1.shape)
         AoD1 = np.r_['1',AoD1,-1*np.inf*np.ones((1,4))]
+
+        AoD2 = 56.4*np.ones(pdb2.shape)
+        AoD2 = np.r_['1',-1*np.inf*np.ones((1,6)),AoD2]
+
+        AoD = np.r_['0',AoD1, AoD2]
+
+        AS_Tx1 = 24.7*np.ones(pdb1.shape)
         AS_Tx1 = np.r_['1',AS_Tx1,-1*np.inf*np.ones((1,4))]
         
-        pdb2 = np.r_['1',-1*np.inf*np.ones((1,6)),pdb2]
-        AoA2 = np.r_['1',-1*np.inf*np.ones((1,6)),AoA2]
-        AS_Rx2 = np.r_['1',-1*np.inf*np.ones((1,6)),AS_Rx2]
-        AoD2 = np.r_['1',-1*np.inf*np.ones((1,6)),AoD2]
+        AS_Tx2 = 22.5*np.ones(pdb2.shape)
         AS_Tx2 = np.r_['1',-1*np.inf*np.ones((1,6)),AS_Tx2]
         
-        pdb = np.r_['0',pdb1, pdb2]
-        AoA = np.r_['0',AoA1, AoA2]
-        AS_Rx = np.r_['0',AS_Rx1, AS_Rx2]
-        AoD = np.r_['0',AoD1, AoD2]
         AS_Tx = np.r_['0',AS_Tx1, AS_Tx2]
+
+        #Reshape pdb's
+        pdb1 = np.r_['1',pdb1,-1*np.inf*np.ones((1,4))]
+        pdb2 = np.r_['1',-1*np.inf*np.ones((1,6)),pdb2]
+        pdb = np.r_['0',pdb1, pdb2]
         
     elif model=='D':
             
         tau = np.array([0,10,20,30,40,50,60,70,80,90,110,140,170,200,240,290,340,390]) * 1e-9
-        tau.reshape((1,-1))
         K=np.zeros(tau.size) #K-factor for Line-of-sight
         K[0]=3
-        pdb1 = np.array([0,-0.9,-1.7,-2.6,-3.5,-4.3,-5.2,-6.1,-6.9,-7.8,-9.0,-11.1,-13.7,-16.3,-19.3,-23.2])
-        pdb2 = np.array([-6.6,-9.5,-12.1,-14.7,-17.4,-21.9,-25.5])
-        pdb3 = np.array([-18.8,-23.2,-25.2,-26.7]) # path losses vector
-        
+
+        pdb1 = np.array([0,-0.9,-1.7,-2.6,-3.5,-4.3,-5.2,-6.1,-6.9,-7.8,-9.0,-11.1,-13.7,-16.3,-19.3,-23.2],ndmin=2)
+
+        pdb2 = np.array([-6.6,-9.5,-12.1,-14.7,-17.4,-21.9,-25.5],ndmin=2)
+
+        pdb3 = np.array([-18.8,-23.2,-25.2,-26.7],ndmin=2) # path losses vector
+
         ASt1 = 27.4*np.ones(pdb1.shape)
+        ASt1  = np.r_['1',ASt1,-1*np.inf*np.ones((1,2))]
+
         ASt2 = 32.1*np.ones(pdb2.shape)
+        ASt2  = np.r_['1', -1*np.inf*np.ones((1,10)), ASt2, -1*np.inf*np.ones((1,1)) ]
         ASt3 = 36.8*np.ones(pdb3.shape)
-        
-        ASt1 = np.array([ASt1,-1*np.inf,-1*np.inf])
-        ASt2 = np.array([-1*np.inf*np.ones((1,10)),ASt2,-1*np.inf])
-        ASt3 = np.array([-1*np.inf*np.ones((1,14)),ASt3])
-        AS_Tx = np.array([ASt1, ASt2, ASt3]) # Tx angular spread vector
+        ASt3  = np.r_['1',-1*np.inf*np.ones((1,14)),ASt3]
+
+        AS_Tx = np.r_['0',ASt1, ASt2, ASt3] # Tx angular spread vector
         
         ASr1 = 27.7*np.ones(pdb1.shape)
+        ASr1 = np.r_['1',ASr1,-1*np.inf*np.ones((1,2))]
+
         ASr2 = 31.4*np.ones(pdb2.shape)
-        ASr3 = 37.4*np.ones(pdb3.shape)
+        ASr2 = np.r_['1',-1*np.inf*np.ones((1,10)),ASr2,-1*np.inf*np.ones((1,1))]
         
-        ASr1 = np.array([ASr1,-1*np.inf,-1*np.inf])
-        ASr2 = np.array([-1*np.inf*np.ones((1,10)),ASr2,-1*np.inf])
-        ASr3 = np.array([-1*np.inf*np.ones((1,14)),ASr3])
-        AS_Rx = np.array([ASr1, ASr2, ASr3]) # Rx angular spread vector
+        ASr3 = 37.4*np.ones(pdb3.shape)
+        ASr3 = np.r_['1',-1*np.inf*np.ones((1,14)),ASr3]
+
+        AS_Rx = np.r_['0',ASr1, ASr2, ASr3] # Rx angular spread vector
         
         AoD1 = 332.1*np.ones(pdb1.shape)
+        AoD1 = np.r_['1',AoD1,-1*np.inf*np.ones((1,2))]
+
         AoD2 = 49.3*np.ones(pdb2.shape)
+        AoD2 = np.r_['1',-1*np.inf*np.ones((1,10)),AoD2,-1*np.inf*np.ones((1,1))]
+
         AoD3 = 275.9*np.ones(pdb3.shape)
-        
-        AoD1 = np.array([AoD1,-1*np.inf,-1*np.inf])
-        AoD2 = np.array([-1*np.inf*np.ones((1,10)),AoD2,-1*np.inf])
-        AoD3 = np.array([-1*np.inf*np.ones((1,14)),AoD3])
-        AoD = np.array([AoD1, AoD2, AoD3]) # Tx angles of departure
+        AoD3 = np.r_['1',-1*np.inf*np.ones((1,14)),AoD3]
+
+        AoD = np.r_['0',AoD1, AoD2, AoD3] # Tx angles of departure
         
         AoA1 = 158.9*np.ones(pdb1.shape)
+        AoA1 = np.r_['1',AoA1,-1*np.inf*np.ones((1,2))]
+
         AoA2 = 320.2*np.ones(pdb2.shape)
+        AoA2 = np.r_['1',-1*np.inf*np.ones((1,10)),AoA2,-1*np.inf*np.ones((1,1))]
+
         AoA3 = 276.1*np.ones(pdb3.shape)
+        AoA3 = np.r_['1',-1*np.inf*np.ones((1,14)),AoA3]
+
+        AoA = np.r_['0',AoA1, AoA2, AoA3] # Rx angles of arrival
+
+        #Reshape pdb's
+        pdb1 = np.r_['1',pdb1,-1*np.inf*np.ones((1,2))]
+        pdb2 = np.r_['1',-1*np.inf*np.ones((1,10)),pdb2,-1*np.inf*np.ones((1,1))]
+        pdb3 = np.r_['1',-1*np.inf*np.ones((1,14)),pdb3]
         
-        AoA1 = np.array([AoA1,-1*np.inf,-1*np.inf])
-        AoA2 = np.array([-1*np.inf*np.ones((1,10)),AoA2,-1*np.inf])
-        AoA3 = np.array([-1*np.inf*np.ones((1,14)),AoA3])
-        AoA = np.array([AoA1, AoA2, AoA3]) # Rx angles of arrival
-        
-        pdb1 = np.array([pdb1,-1*np.inf,-1*np.inf])
-        pdb2 = np.array([-1*np.inf*np.ones((1,10)),pdb2,-1*np.inf])
-        pdb3 = np.array([-1*np.inf*np.ones((1,14)),pdb3])
-        pdb = np.array([pdb1,pdb2,pdb3]) # path loss vector
+        pdb = np.r_['0',pdb1,pdb2,pdb3] # path loss vector
         
     elif model=='E':
         
         tau = np.array([0,10,20,30,50,80,110,140,180,230,280,330,380,430,490,560,640,730]) * 1e-9
-        tau.reshape((1,-1))
         K=np.zeros(tau.size) #K-factor for Line-of-sight
         K[0]=6
-        pdb1 = np.array([-2.6,-3.0,-3.5,-3.9,-4.5,-5.6,-6.9,-8.2,-9.8,-11.7,-13.9,-16.1,-18.3,-20.5,-22.9])
+
+        pdb1 = np.array([-2.6,-3.0,-3.5,-3.9,-4.5,-5.6,-6.9,-8.2,-9.8,-11.7,-13.9,-16.1,-18.3,-20.5,-22.9],ndmin=2)
+        
+        pdb2 = np.array([-1.8,-3.2,-4.5,-5.8,-7.1,-9.9,-10.3,-14.3,-14.7,-18.7,-19.9,-22.4],ndmin=2)
+
+        pdb3 = np.array([-7.9,-9.6,-14.2,-13.8,-18.6,-18.1,-22.8],ndmin=2)
+
+        pdb4 = np.array([-20.6,-20.5,-20.7,-24.6],ndmin=2)
+
+
         AoA1 = 163.7*np.ones(pdb1.shape)
-        AS_Rx1 = 35.8*np.ones(pdb1.shape)
-        AoD1 = 105.6*np.ones(pdb1.shape)
-        AS_Tx1 = 36.1*np.ones(pdb1.shape)
+        AoA1 = np.r_['1',AoA1,-1*np.inf*np.ones((1,3))]
         
-        pdb2 = np.array([-1.8,-3.2,-4.5,-5.8,-7.1,-9.9,-10.3,-14.3,-14.7,-18.7,-19.9,-22.4])
         AoA2 = 251.8*np.ones(pdb2.shape)
-        AS_Rx2 = 41.6*np.ones(pdb2.shape)
-        AoD2 = 293.1*np.ones(pdb2.shape)
-        AS_Tx2 = 42.5*np.ones(pdb2.shape)
-        
-        pdb3 = np.array([-7.9,-9.6,-14.2,-13.8,-18.6,-18.1,-22.8])
+        AoA2 = np.r_['1',-1*np.inf*np.ones((1,4)),AoA2,-1*np.inf*np.ones((1,2))]
+
         AoA3 = 80.0*np.ones(pdb3.shape)
-        AS_Rx3 = 37.4*np.ones(pdb3.shape)
-        AoD3 = 61.9*np.ones(pdb3.shape)
-        AS_Tx3 = 38.0*np.ones(pdb3.shape)
-        
-        pdb4 = np.array([-20.6,-20.5,-20.7,-24.6])
+        AoA3 = np.r_['1',-1*np.inf*np.ones((1,8)),AoA3,-1*np.inf*np.ones((1,3))]
+
         AoA4 = 182.0*np.ones(pdb4.shape)
+        AoA4 = np.r_['1',-1*np.inf*np.ones((1,14)),AoA4]
+        
+        AoA = np.r_['0',AoA1, AoA2, AoA3, AoA4]
+
+        AS_Rx1 = 35.8*np.ones(pdb1.shape)
+        AS_Rx1 = np.r_['1',AS_Rx1,-1*np.inf*np.ones((1,3))]
+        
+        AS_Rx2 = 41.6*np.ones(pdb2.shape)
+        AS_Rx2 = np.r_['1',-1*np.inf*np.ones((1,4)),AS_Rx2,-1*np.inf*np.ones((1,2))]
+
+        AS_Rx3 = 37.4*np.ones(pdb3.shape)
+        AS_Rx3 = np.r_['1',-1*np.inf*np.ones((1,8)),AS_Rx3,-1*np.inf*np.ones((1,3))]
+
         AS_Rx4 = 40.3*np.ones(pdb4.shape)
+        AS_Rx4 = np.r_['1',-1*np.inf*np.ones((1,14)),AS_Rx4]
+
+        AS_Rx = np.r_['0',AS_Rx1, AS_Rx2, AS_Rx3, AS_Rx4]
+
+        AoD1 = 105.6*np.ones(pdb1.shape)
+        AoD1 = np.r_['1',AoD1,-1*np.inf*np.ones((1,3))]
+
+        AoD2 = 293.1*np.ones(pdb2.shape)
+        AoD2 = np.r_['1',-1*np.inf*np.ones((1,4)),AoD2,-1*np.inf*np.ones((1,2))]
+        
+        AoD3 = 61.9*np.ones(pdb3.shape)
+        AoD3 = np.r_['1',-1*np.inf*np.ones((1,8)),AoD3,-1*np.inf*np.ones((1,3))]
+
         AoD4 = 275.7*np.ones(pdb4.shape)
+        AoD4 = np.r_['1',-1*np.inf*np.ones((1,14)),AoD4]
+
+        AoD = np.r_['0',AoD1, AoD2, AoD3, AoD4]
+
+        AS_Tx1 = 36.1*np.ones(pdb1.shape)
+        AS_Tx1 = np.r_['1',AS_Tx1,-1*np.inf*np.ones((1,3))]
+        
+        AS_Tx2 = 42.5*np.ones(pdb2.shape)
+        AS_Tx2 = np.r_['1',-1*np.inf*np.ones((1,4)),AS_Tx2,-1*np.inf*np.ones((1,2))]
+        
+        AS_Tx3 = 38.0*np.ones(pdb3.shape)
+        AS_Tx3 = np.r_['1',-1*np.inf*np.ones((1,8)),AS_Tx3,-1*np.inf*np.ones((1,3))]
+        
         AS_Tx4 = 38.7*np.ones(pdb4.shape)
+        AS_Tx4 = np.r_['1',-1*np.inf*np.ones((1,14)),AS_Tx4]
         
-        pdb1 = np.array([pdb1,-1*np.inf*np.ones((1,3))])
-        AoA1 = np.array([AoA1,-1*np.inf*np.ones((1,3))])
-        AS_Rx1 = np.array([AS_Rx1,-1*np.inf*np.ones((1,3))])
-        AoD1 = np.array([AoD1,-1*np.inf*np.ones((1,3))])
-        AS_Tx1 = np.array([AS_Tx1,-1*np.inf*np.ones((1,3))])
-        
-        pdb2 = np.array([-1*np.inf*np.ones((1,4)),pdb2,-1*np.inf*np.ones((1,2))])
-        AoA2 = np.array([-1*np.inf*np.ones((1,4)),AoA2,-1*np.inf*np.ones((1,2))])
-        AS_Rx2 = np.array([-1*np.inf*np.ones((1,4)),AS_Rx2,-1*np.inf*np.ones((1,2))])
-        AoD2 = np.array([-1*np.inf*np.ones((1,4)),AoD2,-1*np.inf*np.ones((1,2))])
-        AS_Tx2 = np.array([-1*np.inf*np.ones((1,4)),AS_Tx2,-1*np.inf*np.ones((1,2))])
-        
-        pdb3 = np.array([-1*np.inf*np.ones((1,8)),pdb3,-1*np.inf*np.ones((1,3))])
-        AoA3 = np.array([-1*np.inf*np.ones((1,8)),AoA3,-1*np.inf*np.ones((1,3))])
-        AS_Rx3 = np.array([-1*np.inf*np.ones((1,8)),AS_Rx3,-1*np.inf*np.ones((1,3))])
-        AoD3 = np.array([-1*np.inf*np.ones((1,8)),AoD3,-1*np.inf*np.ones((1,3))])
-        AS_Tx3 = np.array([-1*np.inf*np.ones((1,8)),AS_Tx3,-1*np.inf*np.ones((1,3))])
-        
-        pdb4 = np.array([-1*np.inf*np.ones((1,14)),pdb4])
-        AoA4 = np.array([-1*np.inf*np.ones((1,14)),AoA4])
-        AS_Rx4 = np.array([-1*np.inf*np.ones((1,14)),AS_Rx4])
-        AoD4 = np.array([-1*np.inf*np.ones((1,14)),AoD4])
-        AS_Tx4 = np.array([-1*np.inf*np.ones((1,14)),AS_Tx4])
-        
-        pdb = np.array([pdb1, pdb2, pdb3, pdb4])
-        AoA = np.array([AoA1, AoA2, AoA3, AoA4])
-        AS_Rx = np.array([AS_Rx1, AS_Rx2, AS_Rx3, AS_Rx4])
-        AoD = np.array([AoD1, AoD2, AoD3, AoD4])
-        AS_Tx = np.array([AS_Tx1, AS_Tx2, AS_Tx3, AS_Tx4])
+        AS_Tx = np.r_['0',AS_Tx1, AS_Tx2, AS_Tx3, AS_Tx4]
+
+        #Reshape pdb's
+        pdb1 = np.r_['1', pdb1,-1*np.inf*np.ones((1,3))]
+        pdb2 = np.r_['1',-1*np.inf*np.ones((1,4)),pdb2,-1*np.inf*np.ones((1,2))]
+        pdb3 = np.r_['1',-1*np.inf*np.ones((1,8)),pdb3,-1*np.inf*np.ones((1,3))]
+        pdb4 = np.r_['1',-1*np.inf*np.ones((1,14)),pdb4]
+
+        pdb = np.r_['0',pdb1, pdb2, pdb3, pdb4]
 
   #  Still missing model F
-  #  elif model=='F':
-  #      pass
+    elif model=='F':
+        print("Channel model F not yet implemented")
+        sys.exit(1)
 
 
     param_dict={'K':K, 'tau':tau, 'pdb':pdb, 'AS_Tx':AS_Tx, 'AoD':AoD, 'AS_Rx':AS_Rx, 'AoA':AoA}
